8dbe88f72e37443a604aae03c2cec8109481bfc7
delfina <delfifenocchio@gmail.com>
2023-03-23 18:13:00 -0300

    ✨ add file to export atoms and molecules

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as Loader } from './Loader';
+export { default as FileTypeDescription } from './FileTypeDescription';
+export { default as Header } from './Header';
+export { default as Snackbar } from './Snackbar';
+export { default as IconButton } from './IconButton';
---
+++ src/components/molecules/index.ts
@@ -0,0 +1,4 @@
+export { default as DragDropZone } from './DragDropZone';
+export { default as Container } from './Container';
+export { default as WebCamera } from './WebCamera';
+export { default as FileUploadForm } from './FileUploadForm';
