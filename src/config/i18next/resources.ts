5f06d8643b7c8fa7c114cffce3881e74d2fd2834
delfina <delfifenocchio@gmail.com>
2023-03-23 02:28:10 -0300

    🌐 configure i18next

---
+++ src/config/i18next/resources.ts
@@ -0,0 +1,37 @@
+import DRAG_DROP_ZONE_EN from './locale/en/dragdropzone.json';
+import DRAG_DROP_ZONE_ES from './locale/es/dragdropzone.json';
+import HEADER_EN from './locale/en/header.json';
+import HEADER_ES from './locale/es/header.json';
+import HOME_EN from './locale/en/home.json';
+import HOME_ES from './locale/es/home.json';
+import LOGIN_EN from './locale/en/login.json';
+import LOGIN_ES from './locale/es/login.json';
+import PAGE_NOT_FOUND_EN from './locale/en/notfound.json';
+import PAGE_NOT_FOUND_ES from './locale/es/notfound.json';
+import UPLOAD_FILE_FORM_EN from './locale/en/uploadfileform.json';
+import UPLOAD_FILE_FORM_ES from './locale/es/uploadfileform.json';
+import WEBCAMERA_EN from './locale/en/webcam.json';
+import WEBCAMERA_ES from './locale/es/webcam.json';
+
+const resources = {
+  en: {
+    dragdrop: DRAG_DROP_ZONE_EN,
+    header: HEADER_EN,
+    home: HOME_EN,
+    login: LOGIN_EN,
+    page_not_found: PAGE_NOT_FOUND_EN,
+    upload_file_form: UPLOAD_FILE_FORM_EN,
+    webcamera: WEBCAMERA_EN,
+  },
+  es: {
+    dragdrop: DRAG_DROP_ZONE_ES,
+    header: HEADER_ES,
+    home: HOME_ES,
+    login: LOGIN_ES,
+    page_not_found: PAGE_NOT_FOUND_ES,
+    upload_file_form: UPLOAD_FILE_FORM_ES,
+    webcamera: WEBCAMERA_ES,
+  },
+};
+
+export default resources;
