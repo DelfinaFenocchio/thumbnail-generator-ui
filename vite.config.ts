c1b0eb62925ff67f30dc924d034806ddad67de8c
delfina <delfifenocchio@gmail.com>
2023-03-26 01:12:38 -0300

    ➕ add dependencies, configure eslint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@atoms': path.resolve(__dirname, './src/components/atoms'),
+      '@hooks': path.resolve(__dirname, './src/helpers/hooks'),
+      '@molecules': path.resolve(__dirname, './src/components/molecules'),
+      '@screens': path.resolve(__dirname, './src/screens'),
+      '@services': path.resolve(__dirname, './src/config/api/services'),
+      '@store': path.resolve(__dirname, './src/store'),
+    },
+  },
+});
