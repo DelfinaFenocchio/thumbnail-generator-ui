7805533da634322b554fd043d6a3433b6870a9ce
delfina <delfifenocchio@gmail.com>
2023-03-27 01:06:36 -0300

    💄 use theme and add styles in webcam component

--- src/components/molecules/WebCamera/styles.ts
+++ src/components/molecules/WebCamera/styles.ts
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
 
-export default styled.div(
-  ({ theme }) => `
-.icon {
-  color: ${theme.palette.primary.light}
-}
-`
-);
+export default styled.div`
+  .icon {
+    color: ${({ theme }) => theme.palette.primary.light};
+  }
+  @media only screen and (max-device-width: 640px) {
+    .webcamCapture {
+      height: 100%;
+      width: 100%;
+    }
+  }
+  @media only screen and (max-device-width: 768px) {
+    .webcamCapture {
+      height: 100%;
+      width: 100%;
+    }
+  }
+`;
