af72232ce1d6c512e48405ed28aecfd2d554a078
delfina <delfifenocchio@gmail.com>
2023-03-26 23:37:34 -0300

    ✨ add auth0 provider and theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from './context';
+import App from './App';
+import './config/i18next';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: `${window.location.origin}/home`,
+      }}
+    >
+      <ThemeProvider>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Auth0Provider>
+  </React.StrictMode>
+);
