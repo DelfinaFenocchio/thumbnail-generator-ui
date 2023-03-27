ad7d112a41738ce82d5af32262265f1a2728542b
delfina <delfifenocchio@gmail.com>
2023-03-27 01:27:54 -0300

    ✨ add color and size props in loader component

--- src/components/atoms/Loader/index.tsx
+++ src/components/atoms/Loader/index.tsx
@@ -4,15 +4,26 @@ import CircularProgress from '@mui/material/CircularProgress';
 interface LoaderProps {
   sx?: SxProps;
   size?: number;
+  color?:
+    | 'inherit'
+    | 'primary'
+    | 'secondary'
+    | 'error'
+    | 'info'
+    | 'success'
+    | 'warning'
+    | undefined;
 }
 
 /**
  * Loader component
  * @param {object} props
  * @param {SxProps} props.sx The system prop that allows defining system overrides as well as additional CSS styles
+ * @param {"inherit" | "primary" | "secondary" | "error" | "info" | "success" | "warning" | undefined} props.color
+ * @param {number} props.size
  */
-const Loader = ({ sx }: LoaderProps): JSX.Element => {
-  return <CircularProgress sx={sx} color={'inherit'} size={25} />;
+const Loader = ({ sx, size = 25, color = 'inherit' }: LoaderProps): JSX.Element => {
+  return <CircularProgress sx={sx} color={color} size={size} />;
 };
 
 export default Loader;
